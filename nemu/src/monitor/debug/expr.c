570e8c32c5a966d1c6ccf9f54a37c267e78bfc1c
tracer-ics2017 <tracer@njuics.org>
2025-03-05 02:55:12 -0800

    >  compile 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:55:12 up  3:41,  1 user,  load average: 0.12, 0.08, 0.02 40f4ab94564fe366e8ecf94f28eca399e9494d25

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -76,6 +76,220 @@ typedef struct token {
 Token tokens[32];
 int nr_token;
 
+//match pair of parentheses
+bool check_parentheses(int p, int q) {
+  if(p >= q) {
+    return false;
+  }
+  if(tokens[p].type != '(' || tokens[q].type != ')'){
+    return false;
+  }
+  int count = 0;
+  for(int i = p + 1; i < q; i++){
+    if(tokens[i].type == '(') {
+      count++;
+    }
+    if(tokens[i].type == ')') {
+      if(count > 0){
+        count--;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  if(count == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// algorithm priority
+typedef struct {
+  int precedence;
+  int assoc;
+} OpInfo;
+
+OpInfo op_table[] = {
+  {1, 1}, // AND (&&)
+  {1, 1}, // OR (||)
+  {2, 1}, // EQ (==)
+  {2, 1}, // NEQ (!=)
+  {3, 1}, // ADD (+)
+  {3, 1}, // SUB (-)
+  {4, 1}, // MUL (*)
+  {4, 1}, // DIV (/)
+  {5, 0}, // NEGATIVE (-)
+  {5, 0}, // DEREF (*)
+  {5, 0}, // LOGICAL NOT (!)
+};
+
+int get_precedence(int op_type) {
+  switch (op_type) {
+    case TK_AND:      return op_table[0].precedence;
+    case TK_OR:       return op_table[1].precedence;
+    case TK_EQ:       return op_table[2].precedence;
+    case TK_NEQ:      return op_table[3].precedence;
+    case '+':         return op_table[4].precedence;
+    case '-':         return op_table[5].precedence;
+    case '*':         return op_table[6].precedence;
+    case '/':         return op_table[7].precedence;
+    case TK_NEGATIVE: return op_table[8].precedence;
+    case TK_DEREF:    return op_table[9].precedence;
+    case '!':         return op_table[10].precedence;
+    default:          return -1;
+  }
+}
+
+int findDominantOp(int p, int q) {
+  int level = 0;
+  int dominant_op = -1;
+  int max_precedence = -1;
+
+  for (int i = p; i < q; i++) {
+      if (tokens[i].type == '(') {
+          level++;
+      }
+      if (tokens[i].type == ')') {
+          level--;
+      }
+
+      if (level == 0) {
+          int precedence = get_precedence(tokens[i].type);
+          
+          if (precedence > max_precedence) {
+              max_precedence = precedence;
+              dominant_op = i;
+          }
+      }
+  }
+
+  if (dominant_op == -1) {
+      printf("ERROR: No dominant operator found\n");
+      assert(0);
+  }
+
+  return dominant_op;
+}
+
+int eval(int p, int q){
+  // Bad expression
+  if(p > q) {
+    printf("Bad expression\n");
+    assert(0);
+  }
+
+  // Single token
+  else if(p == q) {
+    int num;
+    switch (tokens[p].type){
+      case TK_NUMBER:
+        sscanf(tokens[p].str, "%d", &num);
+        return num;
+      case TK_HEX:
+        sscanf(tokens[p].str, "%x", &num);
+        return num;
+      case TK_REG:
+        if(strcmp(tokens[p].str, "eip") == 0) {
+          return cpu.eip;
+        }
+        for(int i = 0; i < 8; i++) {
+          if(strcmp(tokens[p].str, regsl[i]) == 0) {
+            return reg_l(i);
+          }
+          if(strcmp(tokens[p].str, regsw[i]) == 0) {
+            return reg_w(i);
+          }
+          if(strcmp(tokens[p].str, regsb[i]) == 0) {
+            return reg_b(i);
+          }
+        }
+        printf("error in TK_REG\n");
+        assert(0);
+    }
+  }
+
+  else if(check_parentheses(p, q) == true) {
+    return eval(p + 1, q - 1);
+  }
+
+  else {
+    int op = findDominantOp(p, q);
+    vaddr_t addr;
+    int result;
+
+    switch (tokens[op].type) {
+      case TK_NEGATIVE:
+        printf("Operator= -\n");
+        printf("Value=%d\n",result);
+        return -eval(p + 1, q);
+      case TK_DEREF:
+        addr = eval(p + 1, q);
+        result = vaddr_read(addr, 4);
+        printf("adddr=%u(0x%x)---->value=%d(0x%08x)\n", addr, addr, result, result);
+        return result;
+      case '!': 
+        result = eval(p + 1, q);
+        printf("Operator= !\n");
+        if(result != 0) {
+          printf("Value=0\n");
+          return 0;
+        }
+        else {
+          printf("Value=1\n");
+          return 1;
+        }
+    }
+
+    int val1 = eval(p, op - 1);
+    int val2 = eval(op + 1, q);
+    switch(tokens[op].type) {
+      case '+':
+        printf("Operator= +\n");
+        printf("Value=%d\n",val1+val2);
+        return val1 + val2;
+      case '-': 
+        printf("Operator= -\n");
+        printf("Value=%d\n",val1-val2);
+        return val1 - val2;
+      case '/':
+        if(val2==0){
+          printf("Error: The right value can not be 0\n");
+          assert(0);
+        }
+        printf("Operator= /\n");
+        printf("Value=%d\n",val1/val2);
+        return val1 / val2;
+      case '*':
+        printf("Operator= *\n");
+        printf("Value=%d\n",val1*val2);
+        return val1 * val2;
+      case TK_EQ:
+        printf("Operator= ==\n");
+        printf("Value=%d\n",val1==val2);
+        return val1 == val2;
+      case TK_NEQ: 
+        printf("Operator= !=\n");
+        printf("Value=%d\n",val1!=val2);
+        return val1 != val2;
+      case TK_AND: 
+        printf("Operator= &&\n");
+        printf("Value=%d\n",val1&&val2);
+        return val1 && val2;
+      case TK_OR: 
+        printf("Operator= ||\n");
+        printf("Value=%d\n",val1||val2);
+        return val1 || val2;
+      default:
+        printf("Error: Invalid operator\n");
+        assert(0);
+    }
+  }
+  return 1;
+}
+
 static bool make_token(char *e) {
   int position = 0;
   int i;
@@ -142,7 +356,27 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
 
-  return 0;
+  for(int i = 0; i < nr_token; i++) {
+    if(i == 0){
+      if(tokens[0].type == '-') {
+        tokens[0].type = TK_NEGATIVE;
+      }
+      if(tokens[0].type == '*') {
+        tokens[0].type = TK_DEREF;
+      }
+    }
+    if(tokens[i].type == '-') { 
+      if(tokens[i - 1].type != TK_NUMBER && tokens[i - 1].type != ')') {
+        tokens[i].type = TK_NEGATIVE;
+      }
+    }
+    if(tokens[i].type == '*') {
+      if(tokens[i - 1].type != TK_NUMBER && tokens[i - 1].type != ')') {
+        tokens[i].type = TK_DEREF;
+      }
+    }
+  }
+  *success = true;
+  return eval(0, nr_token - 1);
 }
