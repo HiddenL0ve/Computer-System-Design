6af375b564b5df0e62d5a54ae893df8a18d67b18
tracer-ics2017 <tracer@njuics.org>
2025-03-26 01:05:34 -0700

    >  compile 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:05:34 up  1:03,  1 user,  load average: 0.33, 0.18, 0.11 db585631053c536ae989cb16267f4a7016137c

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,29 +25,25 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  // TODO();
   decoding.is_jmp=1;
-
-  rtl_li(&t2,decoding.seq_eip);
-  rtl_push(&t2);
+  rtl_push(&decoding.seq_eip);  // 絞pc揤梬
+  //cpu.eip=decoding.jmp_eip;  // eip硌砃泐蛌醴梓華硊
 
   print_asm("call %x", decoding.jmp_eip);
 }
 
-make_EHelper(ret) {
-  // TODO();
-  rtl_pop(&t2);
-  decoding.jmp_eip = t2;
-  decoding.is_jmp = 1;
+make_EHelper(ret) {  
+  rtl_pop(&t1);
+  decoding.jmp_eip=t1;  // 泐蛌善梬階暮翹腔殿隙華硊
+  decoding.is_jmp=1;
+
   print_asm("ret");
 }
 
 make_EHelper(call_rm) {
-  // TODO();
-  rtl_li(&t2,decoding.seq_eip);
-  rtl_push(&t2);
-  decoding.jmp_eip=id_dest->val;
   decoding.is_jmp=1;
+  decoding.jmp_eip = id_dest->val;
+  rtl_push(&decoding.seq_eip);
 
   print_asm("call *%s", id_dest->str);
 }
