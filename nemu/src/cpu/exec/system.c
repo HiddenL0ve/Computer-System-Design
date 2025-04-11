091190e8f720c2c4c263732d0d5bcfbdd9fcfd74
tracer-ics2017 <tracer@njuics.org>
2025-04-11 06:55:41 -0700

    >  compile 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  06:55:41 up 41 min,  1 user,  load average: 0.03, 0.02, 0.01 e89f3996193891e148fad64e3a0a2cdcec81867c

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -3,6 +3,8 @@
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 
+void raise_intr(uint8_t NO, vaddr_t ret_addr);
+
 make_EHelper(lidt) {
   // TODO();
   t1 = id_dest -> val;
@@ -38,7 +40,9 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  TODO();
+  //TODO();
+  uint8_t NO = id_dest -> val & 0xff;
+  raise_intr(NO, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
 
