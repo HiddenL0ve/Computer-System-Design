acc32282774a0f80c602c050e5d644eda8eb2d20
tracer-ics2017 <tracer@njuics.org>
2025-04-15 03:41:16 -0700

    >  compile 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:41:16 up 9 min,  1 user,  load average: 0.17, 0.27, 0.24 d080c722c3ff45e07e1dbcbfea4c38ad2ab4a3e

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,22 +6,30 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  //TODO();
-  rtl_push(&cpu.eflags.val);
-
-  cpu.eflags.IF = 0;
-
+  //1. 当前状态压栈
+  memcpy(&t1, &cpu.eflags, sizeof(cpu.eflags));
+  rtl_li(&t0, t1);
+  rtl_push(&t0);
   rtl_push(&cpu.cs);
-  rtl_push(&ret_addr);
-  
-  rtl_li(&t0,vaddr_read(cpu.idtr.base+8*NO,4));
-  rtl_li(&t1,vaddr_read(cpu.idtr.base+8*NO+4,4));
+  rtl_li(&t0, ret_addr);
+  rtl_push(&t0);
 
-  if((t1 & 0x00008000) == 0)
-      assert(0);
-      
-  decoding.jmp_eip = (t0&0xffff)|(t1&0xffff0000);
+  //2. 从intr中读取首地址
+  vaddr_t gate_addr = cpu.idtr.base + NO * sizeof(GateDesc);
+  assert(gate_addr <= cpu.idtr.base + cpu.idtr.limit);
+
+  //3. 读取门描述符
+  uint32_t off_15_0 = vaddr_read(gate_addr,2);
+  uint32_t off_32_16 = vaddr_read(gate_addr+sizeof(GateDesc)-2,2);
+  
+  //4. 计算目标地址
+  uint32_t target_addr = (off_32_16 << 16) + off_15_0;
+#ifdef DEBUG
+  Log("target_addr=0x%x",target_addr);
+#endif
+  //5. 跳转到目标地址
   decoding.is_jmp = 1;
+  decoding.jmp_eip = target_addr;
 }
 
 void dev_raise_intr() {
