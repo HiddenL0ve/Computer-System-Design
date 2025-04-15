718c9f762125276b8fe806b57bfe8280d754e9d0
tracer-ics2017 <tracer@njuics.org>
2025-04-15 08:56:01 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  08:56:01 up 56 min,  1 user,  load average: 0.04, 0.11, 0.06 a5cbd320d7d28ab6b27bd04c1fc09b1f5fb3e8aa

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -37,12 +37,9 @@ static inline _RegSet* sys_brk(_RegSet *r) {
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  a[1] = SYSCALL_ARG2(r);
-  a[2] = SYSCALL_ARG3(r);
-  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
-    case SYS_none:return sys_none(r);
+    case SYS_none: return sys_none(r);
     case SYS_exit:return sys_exit(r);
     case SYS_write:return sys_write(r);
     case SYS_brk:return sys_brk(r);
