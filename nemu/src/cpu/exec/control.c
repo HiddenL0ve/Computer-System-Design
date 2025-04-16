08590f349c42334280d7f34e515aae7f92d23a4c
tracer-ics2017 <tracer@njuics.org>
2025-04-16 07:19:37 -0700

    >  compile 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  07:19:37 up  1:56,  1 user,  load average: 2.05, 2.50, 2.62 2bbe26d2b889ccda5d35d48d6f6c14307627f2c2

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,28 +25,25 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  //TODO();
-  rtl_li(&t2, decoding.seq_eip);
-  rtl_push(&t2);
+  // fisrt push eip
+  rtl_push(eip);
+  // set the destation address jump to
+  rtl_addi(&decoding.jmp_eip, eip, id_dest->val);
+  // set jump flag
   decoding.is_jmp = 1;
-
   print_asm("call %x", decoding.jmp_eip);
 }
 
 make_EHelper(ret) {
-  // TODO();
-  
-  rtl_pop(&t2);
-  decoding.jmp_eip = t2;
+  // only need to pop eip and set jump flag
+  rtl_pop(&decoding.jmp_eip);
   decoding.is_jmp = 1;
   print_asm("ret");
 }
 
 make_EHelper(call_rm) {
-  // TODO();
-  rtl_li(&t2, decoding.seq_eip);
-  rtl_push(&t2);
-  decoding.jmp_eip = id_dest -> val;
+  rtl_push(eip);
+  decoding.jmp_eip=id_dest->val;
   decoding.is_jmp = 1;
   print_asm("call *%s", id_dest->str);
 }
