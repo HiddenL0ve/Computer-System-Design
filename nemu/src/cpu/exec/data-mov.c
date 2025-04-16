ee24859a95129dab2a60a31bf766c208e0939dc8
tracer-ics2017 <tracer@njuics.org>
2025-04-16 07:26:27 -0700

    >  compile 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  07:26:27 up  2:03,  1 user,  load average: 2.30, 2.39, 2.54 cb58e96154fdb849b57a6a43195ba991f8d15

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -14,8 +14,8 @@ make_EHelper(push) {
 
 make_EHelper(pop) {
   // TODO();
-  rtl_pop(&t0);
-  operand_write(id_dest, &t0);
+  rtl_pop(&t2);
+  operand_write(id_dest, &t2);
 
   print_asm_template1(pop);
 }
