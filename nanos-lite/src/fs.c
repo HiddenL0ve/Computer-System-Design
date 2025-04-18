08be1e0847450548a7f94d154ff8de0e528d1797
tracer-ics2017 <tracer@njuics.org>
2025-04-18 02:35:53 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:35:53 up  4:21,  1 user,  load average: 0.94, 0.50, 0.27 58bec71ea4fa4d2166176e0196491d43e2ac32b2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -54,7 +54,7 @@ ssize_t fs_read(int fd, void *buf, size_t len){
   ssize_t fs_size = fs_filesz(fd);
 	if (file_table[fd].open_offset + len > fs_size)
 		len = fs_size - file_table[fd].open_offset;
-  if(fd < 3 || fd >= FD_FB) {
+  if(fd < 3 || fd == FD_FB) {
     Log("arg invalid:fd<3");
     return 0;
   }
@@ -76,7 +76,7 @@ ssize_t fs_read(int fd, void *buf, size_t len){
 ssize_t fs_write(int fd, const void *buf, size_t len){
   assert(fd >= 0 && fd < NR_FILES);
   ssize_t fs_size = fs_filesz(fd);
-  if(fd < 3 || fd >= FD_DISPINFO) {
+  if(fd < 3 || fd == FD_DISPINFO) {
     Log("arg invalid:fd<3");
     return 0;
   }
