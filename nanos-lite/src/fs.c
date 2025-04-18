109382fe318220434175aaff0905417064463c59
tracer-ics2017 <tracer@njuics.org>
2025-04-18 02:35:36 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:35:36 up  4:21,  1 user,  load average: 0.75, 0.43, 0.25 4dc08cd981c06feaa2c80c3c758907b7b36e0e8c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -52,9 +52,9 @@ int fs_open(const char*filename, int flags, int mode) {
 ssize_t fs_read(int fd, void *buf, size_t len){
   //assert(fd >= 0 && fd < NR_FILES);
   ssize_t fs_size = fs_filesz(fd);
-  if (file_table[fd].open_offset + len > fs_size)
-    len = fs_size - file_table[fd].open_offset;
-  if(fd < 3 || fd == FD_FB) {
+	if (file_table[fd].open_offset + len > fs_size)
+		len = fs_size - file_table[fd].open_offset;
+  if(fd < 3 || fd >= FD_FB) {
     Log("arg invalid:fd<3");
     return 0;
   }
@@ -76,7 +76,7 @@ ssize_t fs_read(int fd, void *buf, size_t len){
 ssize_t fs_write(int fd, const void *buf, size_t len){
   assert(fd >= 0 && fd < NR_FILES);
   ssize_t fs_size = fs_filesz(fd);
-  if(fd < 3 || fd == FD_FB) {
+  if(fd < 3 || fd >= FD_DISPINFO) {
     Log("arg invalid:fd<3");
     return 0;
   }
@@ -94,27 +94,26 @@ ssize_t fs_write(int fd, const void *buf, size_t len){
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence) {
-	off_t result = -1;
-	switch(whence) {
-		case SEEK_SET:
-			if (offset >= 0 && offset <= file_table[fd].size) {
-				file_table[fd].open_offset = offset;
-				result = file_table[fd].open_offset;
-			}
-			break;
-		case SEEK_CUR:
-			if ((offset + file_table[fd].open_offset >= 0) && (offset + file_table[fd].open_offset <= file_table[fd].size)) {
-				file_table[fd].open_offset += offset;
-				result = file_table[fd].open_offset;
-			}
-			break;
-		case SEEK_END:
-			file_table[fd].open_offset = file_table[fd].size + offset;
-			result = file_table[fd].open_offset;
-			break;
-	}
-	Log("Seek file [%d] to %d", fd, result);
-	return result;
+  switch(whence) {
+    case SEEK_SET:
+      if (offset >= 0 && offset <= file_table[fd].size){
+        file_table[fd].open_offset = offset;
+        return file_table[fd].open_offset;
+      }
+
+    case SEEK_CUR:
+      if ((offset + file_table[fd].open_offset >= 0) && (offset + file_table[fd].open_offset <= file_table[fd].size)){
+        file_table[fd].open_offset = offset;
+        return file_table[fd].open_offset;
+      }
+
+    case SEEK_END:
+      file_table[fd].open_offset = file_table[fd].size + offset;
+      return file_table[fd].open_offset;
+    default:
+      panic("Unhandled whence ID = %d", whence);
+      return -1;
+    }
 }
 
 int fs_close(int fd) {
