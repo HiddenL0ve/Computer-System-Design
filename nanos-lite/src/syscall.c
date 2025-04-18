f7a7b472009c5b934cbe620ca9f744e3f92c2d02
tracer-ics2017 <tracer@njuics.org>
2025-04-18 03:52:51 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:52:51 up  4:55,  1 user,  load average: 0.75, 0.51, 0.32 4e90bdd281947c32d60639d513dfb1a570c20bac

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -72,12 +72,14 @@ static inline _RegSet* sys_close(_RegSet *r) {
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  Log("enter syscall");
+  //Log("enter syscall");
   switch (a[0]) {
     case SYS_none: 
       Log("enter none");
       return sys_none(r);
-    case SYS_exit: return sys_exit(r);
+    case SYS_exit: 
+      Log("enter none");
+      return sys_exit(r);
     case SYS_write:
       Log("enter write");
       return sys_write(r);
