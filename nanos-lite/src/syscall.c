3554ad09643af7964b09e85e6272618ee5658caa
tracer-ics2017 <tracer@njuics.org>
2025-04-18 01:17:56 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:17:56 up  3:03,  1 user,  load average: 0.00, 0.04, 0.06 d2e77a0fb8176b39fe7127102abf168dd1e303c8

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,6 @@
 #include "common.h"
 #include "syscall.h"
-
+#include "fs.h"
 
 extern void _halt(int);
 
@@ -25,6 +25,9 @@ static inline _RegSet* sys_write(_RegSet *r){
     }
     SYSCALL_ARG1(r) = len;
   }
+  if(fd>=3){
+    SYSCALL_ARG1(r) = fs_write(fd,buf,len);
+  }
   else {
     panic("Unhandled fd=%d in sys_write()", fd);
   }
@@ -34,6 +37,35 @@ static inline _RegSet* sys_write(_RegSet *r){
 
 static inline _RegSet* sys_brk(_RegSet *r) {
   SYSCALL_ARG1(r) = 0;
+  return NULL;
+}
+
+static inline _RegSet* sys_open(_RegSet *r) {
+  const char* pathname = (const char*)SYSCALL_ARG2(r);
+  int flags = (int)SYSCALL_ARG3(r);
+  int mode = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_open(pathname,flags,mode);
+  return NULL;
+}
+static inline _RegSet* sys_read(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int count = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_read(fd,buf,count);
+  return NULL;
+}
+
+static inline _RegSet* sys_lseek(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  off_t offset = (off_t)SYSCALL_ARG3(r);
+  int whence = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_lseek(fd,offset,whence);
+  return NULL;
+}
+
+static inline _RegSet* sys_close(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  SYSCALL_ARG1(r) = fs_close(fd);
   return NULL;
 }
 
@@ -48,6 +80,11 @@ _RegSet* do_syscall(_RegSet *r) {
       //Log("enter write");
       return sys_write(r);
     case SYS_brk:return sys_brk(r);
+    case SYS_open:return sys_open(r);
+    case SYS_read:return sys_read(r);
+    case SYS_close:return sys_close(r);
+    case SYS_lseek:return sys_lseek(r);
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
