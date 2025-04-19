84294657f59defe53be4bafbf2ff0da6826e55ce
tracer-ics2017 <tracer@njuics.org>
2025-04-19 00:12:09 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:12:09 up  1:35,  1 user,  load average: 0.22, 0.22, 0.14 688570989c17db38bbd2313856bfbec831533c7

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,84 +2,104 @@
 #include "syscall.h"
 #include "fs.h"
 
-int sys_none() {
-  return 1;
+extern void _halt(int);
+
+static inline _RegSet* sys_none(_RegSet *r){
+  SYSCALL_ARG1(r) = 1;
+  return NULL;
 }
 
-void sys_exit(int a){
-  _halt(a);
+static inline _RegSet* sys_exit(_RegSet *r){
+  _halt(SYSCALL_ARG2(r)); 
+  return NULL;
 }
 
-int sys_write(int fd, void *buf, size_t len) {
-	if(fd == 1 || fd == 2){
-		char c;
-//		Log("buffer:%s",(char*)buf);
-		for(int i = 0; i < len; i++) {
-			memcpy(&c ,buf + i, 1);
-			_putc(c);
-		}
-		return len;
-	}
-  	if(fd>=3){
-  		return fs_write(fd,buf,len);
-  	}
-  	Log("fd <=0");
-	return -1;			
+static inline _RegSet* sys_write(_RegSet *r){
+  int fd = SYSCALL_ARG2(r);
+  void *buf = (void*)SYSCALL_ARG3(r);
+  size_t len = SYSCALL_ARG4(r);
+  //Log("buffer:%s", (char*)buf);
+  if(fd == 1 || fd == 2) {
+    for(int i = 0; i < len; i++) {
+      _putc(((char*)buf)[i]);
+    }
+    SYSCALL_ARG1(r) = len;
+  }
+  
+  else {
+    SYSCALL_ARG1(r) = fs_write(fd,buf,len);
+    //panic("Unhandled fd=%d in sys_write()", fd);
+  }
+
+  return NULL;
 }
 
-int sys_open(const char *pathname){
-    return fs_open(pathname, 0, 0);
+static inline _RegSet* sys_brk(_RegSet *r) {
+  SYSCALL_ARG1(r) = 0;
+  return NULL;
 }
 
-int sys_read(int fd, void *buf,size_t len){
-    return fs_read(fd, buf, len);
+static inline _RegSet* sys_open(_RegSet *r) {
+  const char* pathname = (const char*)SYSCALL_ARG2(r);
+  int flags = (int)SYSCALL_ARG3(r);
+  int mode = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_open(pathname,flags,mode);
+  return NULL;
 }
 
-int sys_lseek(int fd, off_t offset, int whence) {
-    return fs_lseek(fd, offset, whence);
+static inline _RegSet* sys_read(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int count = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_read(fd,buf,count);
+  return NULL;
 }
 
-int sys_brk(int addr) {
-  return 0;
+static inline _RegSet* sys_lseek(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  off_t offset = (off_t)SYSCALL_ARG3(r);
+  int whence = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_lseek(fd,offset,whence);
+  return NULL;
 }
 
-int sys_close(int fd){
-    return fs_close(fd);
+static inline _RegSet* sys_close(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  SYSCALL_ARG1(r) = fs_close(fd);
+  return NULL;
 }
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  a[1] = SYSCALL_ARG2(r);
-  a[2] = SYSCALL_ARG3(r);
-  a[3] = SYSCALL_ARG4(r);
-
+  //Log("enter syscall");
   switch (a[0]) {
     case SYS_none: 
-      SYSCALL_ARG1(r) = sys_none();
-      break;
+      Log("enter none");
+      return sys_none(r);
     case SYS_exit: 
-      sys_exit(a[1]);
-      break;
+      Log("enter exit");
+      return sys_exit(r);
     case SYS_write:
-      SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
-      break;
+      Log("enter write");
+      return sys_write(r);
     case SYS_brk:
-      SYSCALL_ARG1(r) = sys_brk(a[1]);
-      break;
-    case SYS_read:
-      SYSCALL_ARG1(r) = sys_read(a[1],(void*)a[2],a[3]);
-      break;
+      Log("enter brk");
+      return sys_brk(r);
     case SYS_open:
-      SYSCALL_ARG1(r) = sys_open((char*) a[1]);
-      break;
+      Log("enter open");
+      return sys_open(r);
+    case SYS_read:
+      Log("enter read");
+      return sys_read(r);
     case SYS_close:
-      SYSCALL_ARG1(r) = sys_close(a[1]);
-      break;
+      Log("enter close");
+      return sys_close(r);
     case SYS_lseek:
-      SYSCALL_ARG1(r)=sys_lseek(a[1],a[2],a[3]);
-      break;
+      Log("enter lseek");
+      return sys_lseek(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
   return NULL;
 }
