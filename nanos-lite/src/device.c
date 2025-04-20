50caacbf116bd540034130a73eb16ce043b1c526
tracer-ics2017 <tracer@njuics.org>
2025-04-20 02:08:12 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:08:12 up 56 min,  1 user,  load average: 0.55, 0.73, 0.48 778ba86c2c5fe5368c1d88ebd3c18c96fe7a450b

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,25 +9,20 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  char buffer[40];
-  int key = _read_key();
-  int down = 0;
-  if(key & 0x8000) {
-      key ^= 0x8000;
-      down = 1;
-  }
+	int key = _read_key();
+	bool down = false;
+	if (key & 0x8000) {
+		key ^= 0x8000;
+		down = true;
+	}
   if(key != _KEY_NONE) {
-     sprintf(buffer, "%s %s\n", down ? "kd": "ku", keyname[key]);
+    sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
   }
   else {
-      sprintf(buffer,"t %d\n", _uptime());
+    unsigned long time = _uptime();
+    sprintf(buf, "t %d\n", time);
   }
-  if(strlen(buffer) <= len) { 
-    strncpy((char*)buf, buffer,strlen(buffer));
-	  return strlen(buffer);
-  } 
-  Log("strlen(event)>len, return 0");
-  return 0;
+  return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
