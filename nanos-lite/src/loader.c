40d5fc13b57be87ca4afa11a5642e2085d234881
tracer-ics2017 <tracer@njuics.org>
2025-04-29 04:29:55 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:29:55 up 45 min,  1 user,  load average: 0.47, 0.49, 0.53 69cca922d60ced06c7a0bc19dfbfb56ff18ba166

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "fs.h"
 
-#define DEFAULT_ENTRY ((void *)0x4000000)
+#define DEFAULT_ENTRY ((void *)0x8048000)
 
 #define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
 
@@ -9,6 +9,7 @@ extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void* new_page(void);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
@@ -19,6 +20,18 @@ uintptr_t loader(_Protect *as, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   Log("filename=%s,fd=%d",filename,fd);
   fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  int f_size = fs_filesz(fd);
+
+  void* pa = DEFAULT_ENTRY;
+  void* va = DEFAULT_ENTRY;
+  while(f_size > 0){
+    pa = new_page();
+    _map(as, va, pa);
+    fs_read(fd, pa, PGSIZE);
+    va += PGSIZE;
+    f_size -= PGSIZE;
+  }
   fs_close(fd);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
