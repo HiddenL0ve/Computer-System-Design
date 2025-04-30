6430c8df21a2663498f466971a3cf92b70d998e9
tracer-ics2017 <tracer@njuics.org>
2025-04-30 04:33:43 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:33:43 up 58 min,  1 user,  load average: 0.40, 0.21, 0.13 cc48c1bc351da20f582cb36ac4ebd60121946259

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -19,17 +19,25 @@ int mm_brk(uint32_t new_brk) {
   if(current->cur_brk==0){
     current->cur_brk=current->max_brk=new_brk;
   }
-  else{
-    if(new_brk > current->max_brk){
-			uintptr_t va = (((uint32_t)(current->max_brk)+0xfff) & ~0xfff); // 4k¶ÔÆë
-	    while(va < new_brk) {
-	      _map(&current->as, (void *)va, (void *)new_page());  // ½¨Á¢Ó³Éä
-			  va += PGSIZE;
-	    }
-      current->max_brk=new_brk;
+  else {
+    if (new_brk > current->max_brk) {
+      // TODO: map memory region [current->max_brk, new_brk)
+      // into address space current->as
+      uint32_t first = PGROUNDUP(current -> max_brk);
+      uint32_t end = PGROUNDDOWN(new_brk);
+      if((new_brk & 0xfff) == 0) {
+        end -= PGSIZE;
+      }
+      for(uint32_t va = first; va <= end; va += PGSIZE){
+        void *pa = new_page();
+        _map(&(current -> as), (void*)va, pa);
+      }
+      current->max_brk = new_brk;
     }
-    current->cur_brk=new_brk;
+
+    current->cur_brk = new_brk;
   }
+
   return 0;
 }
 
