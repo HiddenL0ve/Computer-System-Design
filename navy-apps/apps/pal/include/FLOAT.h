ed86e8b59c6b23a58aaa82e8f4006048e7bb9417
tracer-ics2017 <tracer@njuics.org>
2025-05-13 05:09:24 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:09:24 up  1:40,  1 user,  load average: 0.35, 0.74, 0.70 f73bc58281b0cb7f94673d783ccfbbb314657747

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -19,19 +19,19 @@ static inline FLOAT int2F(int a) {
     return -((-a) << 16);
 }
 
-static inline FLOAT F_mul_int(FLOAT a, int b) {
-  return a*b;
-}
-
-static inline FLOAT F_div_int(FLOAT a, int b) {
-  return a/b;
-}
-
 FLOAT f2F(float);
 FLOAT F_mul_F(FLOAT, FLOAT);
 FLOAT F_div_F(FLOAT, FLOAT);
 FLOAT Fabs(FLOAT);
 FLOAT Fsqrt(FLOAT);
 FLOAT Fpow(FLOAT, FLOAT);
+
+static inline FLOAT F_mul_int(FLOAT a, int b) {
+  return F_mul_F(a, int2F(b));
+}
+
+static inline FLOAT F_div_int(FLOAT a, int b) {
+  return F_div_F(a, int2F(b));
+}
 
 #endif
