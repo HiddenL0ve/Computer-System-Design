3292b653324aeff327d30c811eed4e1f29992251
tracer-ics2017 <tracer@njuics.org>
2025-05-13 05:37:52 -0700

    >  run 2211349 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:37:52 up  2:08,  1 user,  load average: 0.16, 0.26, 0.42 2257178cc4d046a53de7e1b0dd92a8405b9b54a9

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -6,32 +6,28 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-  if ((a & 0x80000000) == 0)
-    return a >> 16;
-  else
-    return -((-a) >> 16);
+  return a >> 16;
 }
 
 static inline FLOAT int2F(int a) {
-  if ((a & 0x80000000) == 0)
-    return a << 16;
-  else
-    return -((-a) << 16);
+  return a << 16;
 }
 
-FLOAT f2F(float);
 FLOAT F_mul_F(FLOAT, FLOAT);
 FLOAT F_div_F(FLOAT, FLOAT);
-FLOAT Fabs(FLOAT);
-FLOAT Fsqrt(FLOAT);
-FLOAT Fpow(FLOAT, FLOAT);
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
   return F_mul_F(a, int2F(b));
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-  return F_div_F(a, int2F(b));
+  F_div_F(a, int2F(b));
 }
 
+FLOAT f2F(float);
+FLOAT Fabs(FLOAT);
+FLOAT Fsqrt(FLOAT);
+FLOAT Fpow(FLOAT, FLOAT);
+
 #endif
+
